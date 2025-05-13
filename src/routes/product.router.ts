effb8b427a58662fabe4c6b6f6f416dfa4d7f1b5
JuanGustah <juangt360graus@gmail.com>
2025-05-13 17:28:07 -0300

    feat: add list products by country route

--- src/routes/product.router.ts
+++ src/routes/product.router.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { CreateProductController } from "../controllers/createProduct.controller";
+import { ListProductByCountryController } from "../controllers/listProductByCountry.controller";
 
 const productRouter = Router();
 
 productRouter.post("/product", CreateProductController);
+productRouter.get("/product/find-by-country", ListProductByCountryController);
 
 export { productRouter };
