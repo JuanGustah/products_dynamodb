04e017541bb6ad468e5c755a48d57c11cbf1d779
JuanGustah <juangt360graus@gmail.com>
2025-05-13 17:27:52 -0300

    feat: add parse list products in listProductByCountryService

--- src/services/product/listProductByCountry.service.ts
+++ src/services/product/listProductByCountry.service.ts
@@ -2,6 +2,7 @@ import { QueryCommand } from "@aws-sdk/client-dynamodb";
 
 import { productTableSchema } from "../../config/product-table.schema";
 import { dynamoDocumentClient } from "../../infra/dynamo";
+import { QueryCommandOutput } from "@aws-sdk/lib-dynamodb";
 
 export interface IlistProduct {
     SKU: string
@@ -18,7 +19,7 @@ export async function listProductByCountryService(queryParams: IlistProduct){
     })
 
     const response = await dynamoDocumentClient.send(comand);
-    return response.Items;
+    return parseListProductsResponse(response);
 }
 
 function parseQueryParamsToAttributeValues(queryParams: IlistProduct){
@@ -39,4 +40,34 @@ function parseQueryParamsToAttributeValues(queryParams: IlistProduct){
     }
 
     return [keyExpression, attributeValues];
+}
+
+function parseListProductsResponse(queryResponse: QueryCommandOutput){
+    if(queryResponse.Count === 0){
+        return {}
+    }
+    
+    const productsResponse = queryResponse.Items;
+
+    const products = productsResponse?.map(item=>{
+        const itemKeys = Object.keys(item);
+        const product: any = {};
+
+        itemKeys.forEach(key=>{
+            const keySchema = item[key];
+            const keySchemaType = Object.keys(keySchema)[0];
+
+            if(keySchemaType == "N"){
+                product[key] = Number(keySchema["N"]);
+            }
+
+            if(keySchemaType == "S"){
+                product[key] = keySchema["S"];
+            }
+        })
+
+        return product;
+    })
+
+    return products;
 }
